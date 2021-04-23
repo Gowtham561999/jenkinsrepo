e0978df789c728983da72ca5ced668651f4b6d0f
Gowtham561999 <82655150+Gowtham561999@users.noreply.github.com>
2021-04-23 12:02:04 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello
+{
+public static void main(String[] args)
+{
+for(int i=1;i<10;i++)
+{
+  System.out.println("new");
+  System.out.println("Sampleprj jenkins");
+  System.out.println("Pipeline Jenkins");
+System.out.println("Modified Data"+i);
+//System.out.println("Git is modified");
+//System.out.println("Jenkins Modified");
+  System.out.println("welcome");
+    System.out.println("Changes done by geetha");
+  System.out.println("Changes done by Kushal");
+}
+}
+}
